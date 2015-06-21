7cde8bb7d6c450efd7b0a70fe35724d3e0b28665
syasu <s-yasuhisa@mte.biglobe.ne.jp>
2015-06-22 00:14:17 +0900

    add lottery.py

---
+++ 20150621/lottery.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+
+def solve(n, m, k):
+  for k1 in k:
+    for k2 in k:
+      for k3 in k:
+        for k4 in k:
+          sum = k1 + k2 + k3 + k4
+          if sum == m:
+            return "Yes"           
+  return "No"
+
+result = solve(3,10,[1,3,5])
+print result
